47b93ab4480c220c2ba3d99ef1a2663d37c3af90
Moazz <moazzemh222@gmail.com>
2022-12-17 22:09:54 +0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        //{
+        //    age(2020, 1990);
+        //    Console.ReadLine();
+        //}
+
+        //public static void age(int currentYear, int dateOfBirth)
+        //{
+        //    Console.WriteLine(currentYear - dateOfBirth);
+        //}
+        {
+            int myAge = age(2020, 1990);
+            if (myAge > 17)
+                Console.WriteLine("Adult");
+            else
+                Console.WriteLine("Not Adult");
+            Console.ReadLine();
+        }
+        public static int age(int currentYear, int dateOfBirth)
+        {
+            return currentYear - dateOfBirth;
+        }
+    }
+}
